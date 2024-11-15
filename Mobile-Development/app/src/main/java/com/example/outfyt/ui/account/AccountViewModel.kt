12e9fddd41efbe65220a9305a7f30bd167fdd8b5
Khalied Nauly Maturino <khalidmaturino@gmail.com>
2024-11-16 00:45:01 +0700

    Fixing Logout Button

--- Mobile-Development/app/src/main/java/com/example/outfyt/ui/account/AccountViewModel.kt
+++ Mobile-Development/app/src/main/java/com/example/outfyt/ui/account/AccountViewModel.kt
@@ -1,12 +1,52 @@
 package com.example.outfyt.ui.account
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.outfyt.data.local.LoginPreferences
+import com.example.outfyt.data.remote.response.LogoutRequest
+import com.example.outfyt.data.remote.retrofit.ApiConfig
+import kotlinx.coroutines.launch
 
 class AccountViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is account Fragment"
+    private val _displayName = MutableLiveData<String?>()
+    val displayName: LiveData<String?> = _displayName
+
+    private val _logoutSuccess = MutableLiveData<Boolean>()
+    val logoutSuccess: LiveData<Boolean> = _logoutSuccess
+
+    fun setDisplayName(name: String?) {
+        _displayName.value = name
     }
-    val text: LiveData<String> = _text
+
+    fun logout(context: Context) {
+        val currentAccessToken = LoginPreferences.getAccessToken(context)
+
+        if (currentAccessToken == null) {
+            val logoutRequest = LogoutRequest(currentAccessToken.toString())
+            val apiService = ApiConfig.api
+
+            viewModelScope.launch {
+                try {
+                    val response = apiService.logout(logoutRequest)
+
+                    if (response.isSuccessful && response.body()?.success == true) {
+                        LoginPreferences.saveLoginState(context, false, null, null)
+                        _logoutSuccess.postValue(true)
+                    } else {
+                        _logoutSuccess.postValue(false)
+                        Log.e("LogoutViewModel", "Logout failed: ${response.message()}")
+                    }
+                } catch (_: Exception) {
+                    _logoutSuccess.postValue(false)
+                }
+            }
+        } else {
+            LoginPreferences.saveLoginState(context, false, null, null)
+            _logoutSuccess.postValue(true)
+        }
+    }
 }
