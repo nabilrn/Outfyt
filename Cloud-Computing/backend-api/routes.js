0a5ed436d7b837d498f62088320d4a8d8e9de3a2
habiboys <nouvalhabibie18@gmail.com>
2024-11-22 19:26:10 +0700

    test

--- Cloud-Computing/backend-api/routes.js
+++ Cloud-Computing/backend-api/routes.js
@@ -1,28 +1,36 @@
-const express = require('express');
-const { auth, getCalendar, uploadImage, scrapeNews } = require('./controller.js');
-const {start, send, stream} = require('./controller/chat.js')
-const { verifyGoogleToken } = require('./auth.js');
-const multer = require('multer');
+const express = require("express");
+const {
+    auth,
+    getCalendar,
+    uploadImage,
+    scrapeNews,
+} = require("./controller.js");
+const { start, send, stream } = require("./controller/chat.js");
+const { verifyGoogleToken } = require("./auth.js");
+const multer = require("multer");
 const router = express.Router();
 
-
-
 const upload = multer({
-    storage: multer.memoryStorage(), 
-  });
-  router.get('/test', async (req, res) => {
-    const respon = { 
-      message: "Server running successfully"
+    storage: multer.memoryStorage(),
+});
+router.get("/test", async (req, res) => {
+    const respon = {
+        message: "Server running successfully...",
     };
     res.json(respon);
-  });
-  
-router.post('/auth', auth);
-router.get('/calendar', verifyGoogleToken, getCalendar);
-router.post('/upload-image',  verifyGoogleToken, upload.single('image'), uploadImage);
-router.get('/news', scrapeNews);
-router.post('/chat/start', start  );
-router.post('/chat/send', send  );
-router.post('/chat/stream', stream  );
+});
+
+router.post("/auth", auth);
+router.get("/calendar", verifyGoogleToken, getCalendar);
+router.post(
+    "/upload-image",
+    verifyGoogleToken,
+    upload.single("image"),
+    uploadImage
+);
+router.get("/news", scrapeNews);
+router.post("/chat/start", start);
+router.post("/chat/send", send);
+router.post("/chat/stream", stream);
 
 module.exports = router;
